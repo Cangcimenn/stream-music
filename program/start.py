54f40f7e42044e705ad4c6c6ddf09fdd095e2a4a
Takichan X <92210483+Cangcimenn@users.noreply.github.com>
2021-11-29 20:03:36 +0700

    Update start.py

--- program/start.py
+++ program/start.py
@@ -53,7 +53,9 @@ async def _human_time_duration(seconds):
 )
 async def start_(client: Client, message: Message):
     await message.reply_text(
-        f""" **
+        f"""✨ **Welcome {message.from_user.mention()} !**\n
+💭 [{BOT_NAME}](https://t.me/{BOT_USERNAME}) **Allows you to play music and video on groups through the new Telegram's video chats!**
+💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
 """,
         reply_markup=InlineKeyboardMarkup(
             [

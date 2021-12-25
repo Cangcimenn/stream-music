10d7581f74de7bea5b8787cd2b2e431cc77e47b2
Takichan X <92210483+Cangcimenn@users.noreply.github.com>
2021-12-25 14:02:06 +0700

    Update music.py

--- program/music.py
+++ program/music.py
@@ -61,7 +61,7 @@ async def play(c: Client, m: Message):
                         InlineKeyboardButton(text="•ᴄʜᴀɴɴᴇʟ•", url=f"https://t.me/Takichanbot")
                     ],
                     [
-                        InlineKeyboardButton(text="•ᴏᴡɴᴇʀ•", url="t.me/{OWNER_NAME}"),
+                        InlineKeyboardButton(text="•ᴏᴡɴᴇʀ•", url="https://t.me/Zxyune"),
                         InlineKeyboardButton(text="•ᴄʟᴏsᴇ•", callback_data="cls")
                     ]
                 ]
@@ -281,7 +281,7 @@ async def stream(c: Client, m: Message):
                         InlineKeyboardButton(text="•ᴄʜᴀɴɴᴇʟ•", url=f"https://t.me/Takichanbot")
                     ],
                     [
-                        InlineKeyboardButton(text="•ᴏᴡɴᴇʀ•", url="t.me/{OWNER_NAME}"),
+                        InlineKeyboardButton(text="•ᴏᴡɴᴇʀ•", url="https://t.me/Zxyune"),
                         InlineKeyboardButton(text="•ᴄʟᴏsᴇ•", callback_data="cls")
                     ]
                 ]
